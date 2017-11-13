38675f9da43e714ef58a445fc8dfe252ce3918d0
Zebulun Arendsee <arendsee@iastate.edu>
2017-11-13 14:56:39 -0600

    Add stub download and filter functions

---
+++ R/download.R
@@ -0,0 +1,54 @@
+#' Download a dataset
+#'
+#' @param x OneKP object
+#' @return character vector of paths to the files that were downloaded
+#' @name download
+NULL
+
+#' @rdname download
+#' @export
+download_assembly <- function(x){
+  stop("NOT IMPLEMENTED")
+}
+
+#' @rdname download
+#' @export
+download_transrate_stats <- function(x){
+  stop("NOT IMPLEMENTED")
+}
+
+#' @rdname download
+#' @export
+download_index <- function(x){
+  stop("NOT IMPLEMENTED")
+}
+
+#' @rdname download
+#' @export
+download_geneID_to_orthoID_map <- function(x){
+  stop("NOT IMPLEMENTED")
+}
+
+#' @rdname download
+#' @export
+download_peptides <- function(x){
+  stop("NOT IMPLEMENTED")
+}
+
+#' @rdname download
+#' @export
+download_nucleotides <- function(x){
+  stop("NOT IMPLEMENTED")
+}
+
+#' @rdname download
+#' @export
+download_filtered_gene_sets <- function(x){
+  stop("NOT IMPLEMENTED")
+}
+
+#' @rdname download
+#' @export
+download_filtered <- function(x){
+  stop("NOT IMPLEMENTED")
+}
---
+++ R/filter.R
@@ -0,0 +1,24 @@
+#' Filter a OneKP object
+#'
+#' @param x OneKP object
+#' @return OneKP object
+#' @name filter
+NULL
+
+#' @rdname filter
+#' @export
+filter_by_code    <- function(x, code)    {
+  stop("NOT IMPLEMENTED")
+}
+
+#' @rdname filter
+#' @export
+filter_by_node    <- function(x, node)    {
+  stop("NOT IMPLEMENTED")
+}
+
+#' @rdname filter
+#' @export
+filter_by_species <- function(x, species) {
+  stop("NOT IMPLEMENTED")
+}
